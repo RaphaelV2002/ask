3eb7e991557c11f898b7e211fba39be4f70dee54
Rafael Vagapov <raphael200280@gmail.com>
2022-07-06 20:34:23 +0400

    g

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import url, include
 urlpatterns = [
 url(r'^$', include('qa.urls')),
 url(r'^login/', include('qa.urls')),
