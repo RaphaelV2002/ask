7e18a903ee0eb38362d39afb615759c1852e32c0
RaphaelV2002 <raphael200280@gmail.com>
2022-07-09 17:31:30 +0400

    dfg

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,12 +1,20 @@
 from django.db import models 
-class Post(models.Model):
+from django.contrib.auth.models import User
+class QuestionManager(models.Manager):                                          
+    def new(self):                                                              
+        return self.order_by('-added_at')                                                        
+    def popular(self):                                                          
+        return self.order_by('-rating')
+class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length=255)
-    content = models.TextField()
-    creation_date = models.DateTimeField(blank=True)
-    def __unicode__(self):
-        return self.title
-    def get_absolute_url(self):
-        return '/post/%d/' % self.pk
-    class Meta:
-        db_table = 'blogposts'
-        ordering = ['-creation_date']
+    text = models.TextField()
+    added_at = models.DateTimeField(blank = True, auto_now_add=True)
+    rating = models.IntegerField(default = 0)
+    author = models.OneToOneField()
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank = True, auto_now_add=True)
+    question = models.OneToOneField()
+    author = models.OneToOneField()
--- init.sh
+++ init.sh
@@ -2,3 +2,8 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
 sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+~/web/ask/manage.py makemigrations
+~/web/ask/manage.py migrate
