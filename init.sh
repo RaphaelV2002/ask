4ce67b1726a0d0f5603ce5a353c49f32bf29a78e
RaphaelV2002 <raphael200280@gmail.com>
2022-07-09 18:09:38 +0400

    j

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
+sudo pip3 install --upgrade django==2.2
+sudo pip3 install pathlib
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
 sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
 sudo /etc/init.d/mysql start
-mysql -uroot -e "create database stepic_web;"
-mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+mysql -uroot -e "create database ask;"
+mysql -uroot -e "grant all privileges on ask.* to 'box'@'localhost' with grant option;"
 ~/web/ask/manage.py makemigrations
 ~/web/ask/manage.py migrate
