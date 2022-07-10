2970b9491bbf682d0ab801a00f944dbd6013f369
RaphaelV2002 <raphael200280@gmail.com>
2022-07-10 13:14:32 +0400

    еа

--- init.sh
+++ init.sh
@@ -1,11 +1,15 @@
-sudo pip3 install --upgrade django==2.2
-sudo pip3 install pathlib
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
-sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
+sudo apt update
+sudo apt install python3.5 -y
+sudo apt install python3.5-dev -y
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo python3 -m pip install gunicorn
+sudo python3 -m pip install django==2.0
+sudo python3 -m pip install mysqlclient
+
 sudo /etc/init.d/mysql start
-mysql -uroot -e "create database ask;"
-mysql -uroot -e "grant all privileges on ask.* to 'box'@'localhost' with grant option;"
-~/web/ask/manage.py makemigrations
-~/web/ask/manage.py migrate
+mysql -uroot -e "CREATE DATABASE stepic_web;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
+
+python3 manage.py makemigrations qa
+python3 manage.py migrate
