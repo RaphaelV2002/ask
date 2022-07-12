c9a021feffd543c6260eb4a9f3e11e025ac5c1c5
RaphaelV2002 <raphael200280@gmail.com>
2022-07-12 14:14:10 +0400

    e

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse 
 from django.core.paginator import Paginator
 from . import views
+from . import models
 def test(request, *args, **kwargs):
     return HttpResponse('200')
 
 def question_list_all(request):
-    questions = views.Question.objects.new()
+    questions = models.Question.objects.new()
     limit = request.GET.get('limit', 10)
     page = request.GET.get('page', 1)
     paginator = Paginator(questions, limit)
