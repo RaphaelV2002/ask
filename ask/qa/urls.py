c3396b924177ea5bcb5016566ada71a4bdf107f6
RaphaelV2002 <raphael200280@gmail.com>
2022-07-12 20:49:45 +0400

    h

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
    re_path(r'^signup/.*', views.test, name='signup'),                                   
    re_path(r'^question/(?P<id>[0-9]+)/$', views.test, name='question'),                 
    re_path(r'^ask/.*', views.test, name='ask'),                                         
-   re_path(r'^popular/.*', views.test, name='popular'),                                 
+   re_path(r'^popular/.*', views.rating_question_list_all, name='popular'),                                 
    re_path(r'^new/.*', views.test, name='new'),                                         
 ]
