c8e50e357c82b2e99100b485a9c6b45e9073d1a2
RaphaelV2002 <raphael200280@gmail.com>
2022-07-12 12:49:59 +0400

    d

--- init.sh
+++ init.sh
@@ -12,6 +12,7 @@ sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE ask;"
 mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'password';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'box'@'localhost' WITH GRANT OPTION;"
+mysql -uroot -e "GRANT RELOAD ON *.* TO 'box'@'localhost';"
 mysql -uroot -e "FLUSH   PRIVILEGES;"
 ~/web/ask/manage.py makemigrations qa
 ~/web/ask/manage.py migrate
