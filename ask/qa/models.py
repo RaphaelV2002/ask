08eb1a68c82975539a6c706678ef8a07d19793d6
RaphaelV2002 <raphael200280@gmail.com>
2022-07-13 14:24:02 +0400

    h

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,7 +4,7 @@ class QuestionManager(models.Manager):
     def new(self):                                                              
         return self.order_by('-added_at')                                                        
     def popular(self):                                                          
-        return self.order_by('-pk')
+        return self.order_by('-rating')
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=255)
