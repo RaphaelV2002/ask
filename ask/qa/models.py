58d7feb4b5edf6df05280065b7993794dd6bbf86
RaphaelV2002 <raphael200280@gmail.com>
2022-07-23 13:12:06 +0400

    b

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -12,10 +12,10 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(blank = True, auto_now_add=True)
     rating = models.IntegerField(default = 0)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='question_like_user')
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #likes = models.ManyToManyField(User, related_name='question_like_user')
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(blank = True, auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
